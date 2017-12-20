93d2ce10055587487e6fbd5cd44480c962c6a66a
Rick Beumers <rick_beumers1994@hotmail.com>
2017-12-20 15:28:44 +0100

    Update FirebaseVeiligebuurtPlugin.m

--- src/ios/FirebaseVeiligebuurtPlugin.m
+++ src/ios/FirebaseVeiligebuurtPlugin.m
@@ -72,7 +72,7 @@
 - (void)setCurrentScreen:(CDVInvokedUrlCommand *)command {
     NSString* name = [command.arguments objectAtIndex:0];
 
-    [FIRAnalytics setScreenName:name screenClass:nil];
+    [FIRAnalytics setScreenName:name screenClass:@"UIViewController"];
 
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
